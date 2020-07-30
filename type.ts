f549916430ebab41f64de365c391b107bf02bdda
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-07-30 16:55:14 +0500

    больше типов

--- type.ts
+++ type.ts
@@ -17,6 +17,8 @@ const numArray2: Array<number> = [1,1,2,3,5,8]
 const contact: [string, number] = ['vitaliy', 123213];
 
 
+
+
 let variable: any = 42;
 
 variable = 'stroke';
@@ -42,3 +44,30 @@ type someType = string | null | undefined| void
 function someFunction (): someType{
     
 }
+
+
+const count:number = 5;
+const isFail:boolean = false;
+const say:string = 'hello all';
+const randomCounts:number[] = [2,2,32131,12,3,313,21];
+const voidArray:[] = [];
+let cards: [string, number] = ['vitaliy', 2727];
+
+enum colors {
+    red,
+    black,
+    green
+}
+
+let background: colors = colors.red;
+
+function testObject(t: object | null):void {};
+testObject({name: 'test'});
+
+let someValue:string = 'this string';
+let strLength: number = (someValue).length;
+
+
+let tuple: [number, string, boolean, any] = [7, "hello", true, 123];
+
+let [a, b, c, ...sd] = tuple;
